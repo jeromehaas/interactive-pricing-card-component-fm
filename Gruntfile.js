63f2da39202968048f95bdc41631c1e0a0829075
Jerome Haas <hello@jeromehaas.dev>
2022-03-05 16:59:05 +0100

    Setup: browsersync with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,147 @@
+// const browserSync = require("browser-sync");
+
+// module.exports = function(grunt) {
+
+// 	const config = {
+		
+// 		uglify: {
+// 			build: {
+//         src: 'src/js/hello-world.js',
+// 				dest: 'build/js/<%= pkg.name %>.min.js'
+// 			}
+// 		},
+	
+// 		less: {
+// 			development: {
+//         options: {
+//           compress: true,
+//           yuicompress: true,
+//           optimization: 2
+//         },
+//         files: {
+//           "css/main.css": "src/less/central.less" 
+//         }
+//   	  }
+// 		},
+		
+// 		watch: {
+// 			css: {
+// 				files: 'src/less/**/*.less',
+// 				tasks: ['lessTask'],
+// 				options: {
+// 					livereload: {
+// 						host: 'localhost',
+// 						port: 3005,
+// 					}
+// 				},
+// 			},
+// 		},
+
+// 	};
+
+// 	grunt.initConfig(config);
+// 	grunt.loadNpmTasks('grunt-contrib-uglify');
+// 	grunt.loadNpmTasks('grunt-contrib-less');
+// 	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+// 	grunt.registerTask('watchTask', function() {
+// 		grunt.task.run(['watch']);
+// 	});
+
+// 	grunt.registerTask('lessTask', function() {
+// 		grunt.task.run(['less']);
+// 	})
+	
+// 	grunt.registerTask('default', function() {
+// 		grunt.log.writeln('----------------------------------');
+// 		grunt.log.writeln('LESS');
+// 		grunt.task.run(['lessTask']);
+// 		grunt.log.writeln('----------------------------------');
+// 		grunt.log.writeln('WATCH');
+// 		grunt.task.run(['watchTask']);
+		
+// 	});
+
+// };
+
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			build: {
-        src: 'src/js/hello-world.js',
-				dest: 'build/js/<%= pkg.name %>.min.js'
-			}
-		},
+	const config = {
+		
 		less: {
-      development: {
+			development: {
         options: {
           compress: true,
           yuicompress: true,
           optimization: 2
         },
         files: {
-          "css/main.css": "src/less/main.less" // destination file and source file
+          "css/main.css": "src/less/central.less" 
         }
-      }
-    },
-    watch: {
-      styles: {
-        files: ['src/less/**/*.less'], // which files to watch
-        tasks: ['less'],
-        options: {
-          nospawn: true
-        }
-      }
-    }
-	})
+  	  }
+		},
+		
+		watch: {
+			less: {
+				files: 'src/less/**/*.less',
+				tasks: ['lessTask'],
+			},
+			html: {
+				files: 'index.html',
+				tasks: ['htmlTask'],
+			},
+			options: {
+				livereload: {
+					host: 'localhost',
+					port: 35729,
+				}
+			}
+		},
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+		connect: {
+			server: {
+				options: {
+					port: 3005,
+					base: '.',
+					hostname: 'localhost',
+					protocol: 'http',
+					livereload: true,
+					open: true,
+				}
+			}
+		},
+
+	};
+
+	grunt.initConfig(config);
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 
+	grunt.registerTask('watchTask', function() {
+		grunt.task.run(['connect', 'watch']);
+	});
 
-grunt.registerTask('test', ['uglify']);
-
-grunt.registerTask('print', function() {
-	const done = this.async();
-	setTimeout(() => {
-		const success = 'success';
-		done(success);
-	}, 1000);
-})
-
-
-	grunt.registerTask('default', () => {
-		grunt.log.writeln('--------');
-		grunt.log.writeln('LESS TASK');
+	grunt.registerTask('lessTask', function() {
+		grunt.log.writeln('----------------------------------');
+		grunt.log.writeln('LESS');
 		grunt.task.run(['less']);
-		grunt.task.run(['print']);
-		grunt.log.writeln('--------');
-		grunt.log.writeln('WATCH TASK');
-		grunt.task.run(['watch']);
-		grunt.task.run(['test']);
+	});
+	
+	grunt.registerTask('htmlTask', function() {
+		grunt.log.writeln('----------------------------------');
+		grunt.log.writeln('HTML');
+	});
+	
+	grunt.registerTask('default', function() {
+		grunt.log.writeln('----------------------------------');
+		grunt.log.writeln('LESS');
+		grunt.task.run(['lessTask']);
+		grunt.log.writeln('----------------------------------');
+		grunt.log.writeln('WATCH');
+		grunt.task.run(['watchTask']);
+		
 	});
 
 };
